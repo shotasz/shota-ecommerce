43dcf122eb820bedc436b590b9f73aa7b41e0daf
shotasz <s2shota123gmail.com>
2021-11-24 21:47:22 +0900

    framework/shopify内のAPIを部分を構築

---
+++ framework/shopify/product/get-all-products.ts
@@ -0,0 +1,10 @@
+import getAllProductsQuery from "../utils/queries/get-all-products";
+import fetchApi from "../utils/fetch-api";
+
+const getAllProducts = async (): Promise<any[]> => {
+  const products = await fetchApi({ query: getAllProductsQuery });
+
+  return products.data;
+};
+
+export default getAllProducts;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,20 @@
-export default function Home() {
-  const message: string = "Hello World !";
+import { useEffect } from "react";
+import type { InferGetStaticPropsType } from "next";
+import getAllProducts from "../framework/shopify/product/get-all-products";
 
-  return <div>{message}</div>;
+export async function getStaticProps() {
+  const products = await getAllProducts();
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 4 * 60 * 60,
+  };
+}
+
+export default function Home({
+  products,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <div>{JSON.stringify(products)}</div>;
 }
