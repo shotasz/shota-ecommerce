502752bec0d6b965510b8cf685fd69469fabed9f
shotasz <s2shota123gmail.com>
2021-12-12 10:20:24 +0900

    tailwindcssの導入

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,7 +1,12 @@
+import styles from "./Layout.module.css";
 import { FC } from "react";
 
 const Layout: FC = ({ children }) => {
-  return <div className="layout">{children}</div>;
+  return (
+    <div className={styles.root}>
+      <main className="fit">{children}</main>
+    </div>
+  );
 };
 
 export default Layout;
