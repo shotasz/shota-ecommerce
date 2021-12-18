a0f858bd860ce5f072c49c477dd1e91e5f375606
shotasz <s2shota123@gmail.com>
2021-12-18 18:03:12 +0900

    CartSidebarの作成

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,11 +1,16 @@
 import styles from "./Layout.module.css";
 import { Footer, Navbar } from "@components/common";
+import { Sidebar } from "@components/ui";
+import { CartSidebar } from "@components/cart";
 import { FC } from "react";
 
 const Layout: FC = ({ children }) => {
   return (
     <div className={styles.root}>
       <Navbar />
+      <Sidebar>
+        <CartSidebar />
+      </Sidebar>
       <main className="fit">{children}</main>
       <Footer />
     </div>
