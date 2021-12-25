dd0d85db3eadd73723f9fe043899fdd4ae2a4964
shotasz <s2shota123@gmail.com>
2021-12-25 19:56:43 +0900

    component/productのProductSliderをコミット

---
+++ components/product/ProductSlider/ProductSlider.tsx
@@ -0,0 +1,57 @@
+import React, { FC, Children, isValidElement, useState } from "react";
+import { useKeenSlider } from "keen-slider/react";
+import styles from "./ProductSlider.module.css";
+import classNames from "classnames";
+
+const ProductSlider: FC = ({ children }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [sliderRef, slider] = useKeenSlider({
+    initial: 0,
+    loop: true,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+  });
+
+  console.log(currentSlide);
+
+  return (
+    <div className={styles.root}>
+      <div
+        ref={sliderRef as React.LegacyRef<HTMLDivElement>}
+        className="keen-slider h-full transition-opacity"
+      >
+        <button
+          onClick={() => slider.current?.prev()}
+          className={classNames(styles.leftControl, styles.control)}
+        />
+        <button
+          onClick={() => slider.current?.next()}
+          className={classNames(styles.rightControl, styles.control)}
+        />
+        {Children.map(children, (child) => {
+          if (isValidElement(child)) {
+            return {
+              ...child,
+              props: {
+                ...child.props,
+                className: `${
+                  child.props.className ? `${child.props.className}` : ""
+                } keen-slider__slide`,
+              },
+            };
+            // return React.cloneElement(child, {
+            //   className: `${
+            //     child.props.className ? `${child.props.className}` : ""
+            //   } keen-slider__slide`,
+            // });
+          }
+
+          return child;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ProductSlider;
