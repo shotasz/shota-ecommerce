4b82849be4b5026a6c8a9e0b909b903b7ce040c2
shotasz <s2shota123@gmail.com>
2022-02-26 09:28:29 +0900

    Hero画面の説明文の変更

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,8 +29,10 @@ export default function Home({
       </Grid>
 
       <Hero
-        headline="Big Time Games"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        headline="Lite-E-Commerce"
+        description="本サイトでは好きな商品の色・サイズを選び、カートに入れ、購入手続きまで対応しています。お気に入り機能や商品別のカテゴリー分けはまだ制作段階ですので、今しばらくお待ち下さい。
+        支払い方法に関してデモンストレーション仕様になっており、実際に支払いすることはありませんのでご安心下さい。
+"
       />
 
       <Marquee>
